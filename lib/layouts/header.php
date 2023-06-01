424252590be9c8c32f3ec186497af5e2bf49d97e
JehanKandy <jehankandy@gmail.com>
2023-06-01 18:53:01 +0530

    Update

--- lib/layouts/header.php
+++ lib/layouts/header.php
@@ -5,9 +5,10 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Online Train Booking System</title>
-    <link rel="icon" type="image/png" href="images/ticket.png">
+    <link rel="icon" type="image/png" href="https://png.pngtree.com/png-clipart/20230126/original/pngtree-train-on-a-white-background-png-image_8930665.png">
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-9ndCyUaIbzAi2FUVXJi0CjmCapSmO7SnpJef0486qhLnuZ2cdeRhO02iuK6FUUVM" crossorigin="anonymous">
 </head>
 <body>
-    
+
+<script src=""></script>
 <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/js/bootstrap.bundle.min.js" integrity="sha384-geWF76RCwLtnZ8qwWowPQNguL3RmwHVBC9FhGdlKrxdiJJigb/j/68SIy3Te4Bkz" crossorigin="anonymous"></script>
