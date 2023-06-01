0db94fcf2c05c5fc083a374bb474570d71978397
JehanKandy <jehankandy@gmail.com>
2023-06-01 18:56:27 +0530

    Update

---
+++ lib/layouts/footer.php
@@ -0,0 +1,14 @@
+<div class="row">
+    <div class="col-sm-3">
+        ni
+    </div>
+    <div class="col-sm-3">
+        ni
+    </div>
+    <div class="col-sm-3">
+        ni
+    </div>
+    <div class="col-sm-3">
+        ni
+    </div>
+</div>
