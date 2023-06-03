b1bbf5c5997d5f643e0fb9e7e0db57dec78ac91a
JehanKandy <jehankandy@gmail.com>
2023-06-03 08:57:05 +0530

    Update

--- lib/layouts/footer.php
+++ lib/layouts/footer.php
@@ -6,6 +6,7 @@
                 <div class="title-col">Online Train Ticket Booking</div>
                 <div class="body-col">
                     <img src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTL2DLbXqb3focnLW3FvFl_cCTlm1xGXyXweg&usqp=CAU" alt="site Logo" class="site-footer-logo">
+                    <p>This system is develop for educational purposes only</p>
                 </div>
             </div>
             <div class="col-sm-3 footer-box">
