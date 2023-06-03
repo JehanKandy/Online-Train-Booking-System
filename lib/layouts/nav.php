4244cd7d86053799e4e1316a25447c00b5fdf775
JehanKandy <jehankandy@gmail.com>
2023-06-03 10:07:01 +0530

    Update

--- lib/layouts/nav.php
+++ lib/layouts/nav.php
@@ -1,3 +1,6 @@
 <nav class="navbar bg-dark border-bottom border-bottom-dark" data-bs-theme="dark">
-
+    <div class="container">
+        <h3>Train Ticket Booking</h3>
+        <button class="btn btn-outline-success" type="submit">Search</button>    
+    </div>
 </nav>
