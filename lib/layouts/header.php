4db1ca989bb2afe30d3c47167c040d8de431450d
JehanKandy <jehankandy@gmail.com>
2023-06-03 10:17:32 +0530

    Update

--- lib/layouts/header.php
+++ lib/layouts/header.php
@@ -9,6 +9,7 @@
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-9ndCyUaIbzAi2FUVXJi0CjmCapSmO7SnpJef0486qhLnuZ2cdeRhO02iuK6FUUVM" crossorigin="anonymous">
     <link rel="stylesheet" href="scss/app.min.css">
     <script src="https://kit.fontawesome.com/a076d05399.js" crossorigin="anonymous"></script>
+    <link rel="stylesheet" href="https://fonts.googleapis.com/css2?family=Material+Symbols+Outlined:opsz,wght,FILL,GRAD@20..48,100..700,0..1,-50..200" />
 </head>
 <body>
 
--- lib/layouts/nav.php
+++ lib/layouts/nav.php
@@ -1,6 +1,6 @@
 <nav class="navbar bg-dark border-bottom border-bottom-dark" data-bs-theme="dark">
     <div class="container">
         <h3 style="color: white;">Train Ticket Booking</h3>
-        <a href=""><button class="btn btn-primary">Login</button></a>
+        <a href=""><button class="btn btn-primary"><span class="login-text">Login</span></button></a>
     </div>
 </nav>
