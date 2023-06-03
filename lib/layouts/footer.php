1369b62ab228d7d766762dd66c033001f7f1fe75
JehanKandy <jehankandy@gmail.com>
2023-06-03 09:04:29 +0530

    Update

--- lib/layouts/footer.php
+++ lib/layouts/footer.php
@@ -7,10 +7,18 @@
                 <div class="body-col">
                     <img src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTL2DLbXqb3focnLW3FvFl_cCTlm1xGXyXweg&usqp=CAU" alt="site Logo" class="site-footer-logo">
                     <p class="footer-p">This system is develop for educational purposes only</p>
+                    <p>The Online Train ticket booking System licensed under <a href="https://github.com/JehanKandy/Online-Train-Booking-System/blob/master/LICENSE" target="_blank">MIT</a></p>
+                    <p>Currently Version : </p>
                 </div>
             </div>
             <div class="col-sm-3 footer-box">
                 <div class="title-col">Services</div>
+                <div class="body-col">
+                    <ul class="footer-list">
+                        <li>Train Booking</li>
+                        <li>Find Train Routes</li>
+                    </ul>
+                </div>
             </div>
             <div class="col-sm-3 footer-box">
                 <div class="title-col">Useful links</div>
